a4b1203b58ed7a33b4050be66e2b180399418072
Bernard Essilfie <bernard.essilfie@amalitech.com>
2025-02-26 13:25:06 +0000

    first commit

---
+++ src/main/java/com/essilfie/HelloElasticBeanstalk/HelloElasticBeanstalkApplication.java
@@ -0,0 +1,13 @@
+package com.essilfie.HelloElasticBeanstalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloElasticBeanstalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloElasticBeanstalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/essilfie/HelloElasticBeanstalk/HelloElasticBeanstalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essilfie.HelloElasticBeanstalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloElasticBeanstalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
