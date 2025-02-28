f6c3089e52019bd83cc3fb4cd3c87f6618587e13
Bernard Essilfie <bernard.essilfie@amalitech.com>
2025-02-28 08:22:29 +0000

    add github actions workflow to upload jar file to s3  bucket

--- src/main/java/com/essilfie/HelloElasticBeanstalk/HelloController.java
+++ src/main/java/com/essilfie/HelloElasticBeanstalk/HelloController.java
@@ -1,12 +1,12 @@
 package com.essilfie.HelloElasticBeanstalk;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello ElasticBeanstalk";
+        return "index";
     }
 }
